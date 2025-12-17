278f890935ec4d8f5589b8666e24754bf42963a0
JYXC-DZ-0101053 <jyxc-dz-0101053@JYXC-DZ-0101053deMacBook-Pro.local>
2025-12-17 11:28:04 +0800

    LVLM_COUNT

--- PASCAL/process.py
+++ PASCAL/process.py
@@ -58,32 +58,6 @@ with open('sampled_pascal.csv', 'r') as f:
         else:
             print(row[0])
 
-# import shutil
-# with open('data.json', 'w') as f:
-#     json.dump(new_data, f, ensure_ascii=False, indent=4)
+with open('data.json', 'w') as f:
+    json.dump(new_data, f, ensure_ascii=False, indent=4)
 
-# # from datasets import load_dataset
-
-# # ds = load_dataset("merve/pascal-voc")
-
-# # print(ds['train'][0])
-
-# # image_name = []
-# # for i, item in enumerate(ds['train']):
-# #     image_name.append(ds['train'][i]['text'])
-# # for i, item in enumerate(ds['validation']):
-# #     image_name.append(ds['validation'][i]['text'])
-
-# # print(len(image_name))
-
-# # import csv
-# # new_data = []
-# # with open('sampled_pascal.csv', 'r') as f:
-# #     reader = csv.reader(f)
-# #     id = 0
-# #     for i, row in enumerate(reader):
-# #         for y in ['2012']:
-# #             if y + '_' + row[0].split('.')[0] in image_name:
-# #                 print('Yes', y + '_' + row[0].split('.')[0])
-# #             else:
-# #                 print(row[0].split('.')[0])
